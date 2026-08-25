c67d022007ce256e3109eb6aed8eaeb5b231ad0c
michh18 <michellechan0518@gmail.com>
2026-08-25 21:09:26 +0100

    Add route for Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import HomePage from "./pages/HomePage";
 import ClinicalNotesPage from "./pages/ClinicalNotesPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/upload" element={<ClinicalNotesPage />} />
       </Routes>
     </BrowserRouter>
